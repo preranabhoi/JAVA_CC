bda945459cb2ba1ec0b1f8945e15d3ff0e3d5c98
Prerana Bhoi <preranabhoi32@gmail.com>
2026-08-05 23:29:55 +0530

    Day-08

---
+++ TripletExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TripletExample {
+   public static void main(String[] args) {
+    ArrayList<Integer> triplet = new ArrayList<>();
+    triplet.add(-1);
+    triplet.add(0);
+    triplet.add(1);
+    System.out.println(triplet);
+   }
+}
